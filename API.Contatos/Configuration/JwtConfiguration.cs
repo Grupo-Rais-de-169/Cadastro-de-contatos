e6c05edf897d9291558083005125011199bccc3d
SEGURADORALIDER\luizmarinho.brq <luizmarinho.brq@seguradoralider.com.br>
2024-11-19 14:39:20 -0300

    Inclusão do gitignore e adição das configurações do JWT e Swagger

---
+++ API.Contatos/Configuration/JwtConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace API.Contatos.Configuration
+{
+    public static class JwtConfiguration
+    {
+        public static WebApplicationBuilder AddJwtConfiguration(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(opt =>
+            {
+                opt.RequireHttpsMetadata = false;
+                opt.SaveToken = true;
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });
+
+            builder.Services.AddAuthorization(opt =>
+            {
+                opt.AddPolicy("Administrador", policy => policy.RequireRole("Administrador"));
+                opt.AddPolicy("Usuario", policy => policy.RequireRole("Usuario"));
+            });
+
+            return builder;
+        }
+    }
+}
