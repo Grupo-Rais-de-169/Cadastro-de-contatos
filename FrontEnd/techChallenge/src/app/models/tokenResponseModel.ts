dfa78f55759a538616d53ae3fae4a396b67511db
beto-frs <fsilva@compugraf.com.br>
2024-11-21 23:35:32 -0300

    feat: Iniciando Front (Angular)

---
+++ FrontEnd/techChallenge/src/app/models/tokenResponseModel.ts
@@ -0,0 +1,11 @@
+import { empty } from "rxjs";
+
+export class tokenResponseModel{  
+        public login!: string;
+        public permissao!: string;
+        public token!: string;
+        public refreshToken! : string;
+        public create!: string;
+        public validate!: string;
+    
+}
