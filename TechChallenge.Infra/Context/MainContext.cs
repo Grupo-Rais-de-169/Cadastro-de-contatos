241f4f07fe322509210b784b32e6236351efe8d4
beto-frs <fsilva@compugraf.com.br>
2024-12-10 23:37:54 -0300

    RepositooryTests

--- TechChallenge.Infra/Context/MainContext.cs
+++ TechChallenge.Infra/Context/MainContext.cs
@@ -2,23 +2,30 @@
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Logging;
 using TechChallenge.Domain;
+using TechChallenge.Domain.Entities;
 
 namespace TechChallenge.Infra.Context
 {
     public class MainContext : DbContext
     {
         private readonly IDbContextFactory<MainContext> _context;
+
         public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<Permissao> Permissao { get; set; }
         public DbSet<Contato> Contatos { get; set; }
         public DbSet<CodigoDeArea> CodigosDeArea { get; set; }
 
-        public MainContext(DbContextOptions<MainContext> options, ILogger<MainContext> logger, IDbContextFactory<MainContext> context) : base(options)
+        public MainContext(DbContextOptions<MainContext> options, IDbContextFactory<MainContext> context) : base(options)
         {
             Database.SetCommandTimeout(1000);
             _context = context;
         }
-        
+
+        public MainContext(DbContextOptions<MainContext> options) : base(options)
+        {
+
+        }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -34,6 +41,14 @@ namespace TechChallenge.Infra.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(MainContext).Assembly);
+
+            // ========================================
+            base.OnModelCreating(modelBuilder);
+
+            // Configura a chave primária para EntityBase
+            modelBuilder.Entity<EntityBase>().HasKey(e => e.Id);
+            modelBuilder.Entity<EntityBase>().ToTable("Entities");
+            modelBuilder.Entity<CodigoDeArea>().HasBaseType<EntityBase>();
         }
     }
 }
