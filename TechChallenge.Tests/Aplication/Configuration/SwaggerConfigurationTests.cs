0da54c6a0d2c3e60156cde85ed8052a69ccb81fd
beto-frs <fsilva@compugraf.com.br>
2024-12-12 02:33:17 -0300

    Initial

---
+++ TechChallenge.Tests/Aplication/Configuration/SwaggerConfigurationTests.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using TechChallenge.Api.Configuration;
+using Xunit;
+using Swashbuckle.AspNetCore.ReDoc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace TechChallenge.Tests.Aplication.Configuration
+{
+    public class SwaggerConfigurationTests
+    {
+        [Fact]
+        public void AddSwaggerConfiguration_ShouldAddSwaggerGen()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+
+            // Act
+            SwaggerConfiguration.AddSwaggerConfiguration(services);
+
+            // Assert
+            var serviceProvider = services.BuildServiceProvider();
+            Assert.NotNull(serviceProvider);
+        }
+    }
+}
