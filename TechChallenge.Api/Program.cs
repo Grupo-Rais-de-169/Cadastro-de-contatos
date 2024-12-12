fef9381d183f3d3373cb606bf06f7fcbe9e8cde4
Thiago <Thiago@DESKTOP-9TMNURV>
2024-12-12 13:18:58 -0300

    dividindo a probram com uma startup e aplicando testes

--- TechChallenge.Api/Program.cs
+++ TechChallenge.Api/Program.cs
@@ -1,122 +1,19 @@
-using AutoMapper;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
-using Microsoft.OpenApi.Models;
-using NLog.Web;
-using TechChallenge.Api.Configuration;
-using TechChallenge.Api.Services;
-using TechChallenge.Domain.Config;
-using TechChallenge.Domain.Interfaces.Repositories;
-using TechChallenge.Domain.Interfaces.Services;
-using TechChallenge.Domain.Services;
-using TechChallenge.Infra;
-using TechChallenge.Infra.Context;
-using TechChallenge.Infra.Repositories;
+using TechChallenge.Api;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-#region [DB]
-builder.Services.AddDbContextFactory<MainContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL")));
-#endregion
-
-builder.Services.AddMemoryCache();
-builder.Services.AddControllers();
-
-#region [DI]
-builder.Services
-    .AddScoped<ITokenServices, TokenServices>()
-    .AddScoped<IAuthService, AuthService>()
-    .AddScoped<IAuthRepositories, AuthRepositories>()
-    .AddScoped<IUsuarioService, UsuarioService>()
-    .AddScoped<IContatoService, ContatoService>()
-    .AddScoped<IPasswordService, PasswordService>()
-    .AddScoped<IContatosRepository, ContatosRepository>()
-    .AddScoped<ICodigoDeAreaRepository, CodigoDeAreaRepository>()
-    .AddScoped<IUsuarioRepository, UsuarioRepository>()
-    .AddScoped<IPermissaoRepository, PermissaoRepository>()
-    .AddSingleton<DbConnectionProvider>();
-#endregion
-
-builder.Services.AddEndpointsApiExplorer();
-builder.AddJwtConfiguration();
-
-builder.Logging.ClearProviders();
-builder.Host.UseNLog();
-
-IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
-builder.Services.AddSingleton(mapper);
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-#region [SWAGGER]
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Api de Contatos",
-        Version = "v1",
-        Description = "Challenge realizado pelo grupo 13",
-        License = new OpenApiLicense
-        {
-            Name = "MIT",
-            Url = new Uri("https://opensource.org/licenses/MIT")
-        }
-    });
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "Insira o token JWT no campo. Exemplo: Bearer {seu token}",
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        In = ParameterLocation.Header
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        Array.Empty<string>()
-                    }
-                });
-});
-#endregion
+builder.ConfigureServices();
 
 var app = builder.Build();
-var cache = app.Services.GetRequiredService<IMemoryCache>();
 
-#region[CACHE]
+var cache = app.Services.GetRequiredService<IMemoryCache>();
 cache.Set("key", "value", new MemoryCacheEntryOptions
 {
     AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1)
 });
-#endregion
-app.UseSwagger();
-app.UseSwaggerUI();
 
-#region[ReDoc]
-app.UseReDoc(c =>
-{
-    c.SpecUrl("/swagger/v1/swagger.json"); // URL do JSON do Swagger
-    c.DocumentTitle = "Documentação da API com ReDoc";
-    c.RoutePrefix = "redoc"; // ReDoc acessível em /redoc
-});
-#endregion
-app.UseCors(x => x
-    .AllowAnyOrigin()
-    .AllowAnyMethod()
-    .AllowAnyHeader());
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
+app.ConfigureMiddleware();
 
 await app.RunAsync();
