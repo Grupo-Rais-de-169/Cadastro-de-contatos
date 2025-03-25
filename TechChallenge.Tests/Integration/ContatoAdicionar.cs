852b096fb7afc72cf46cef431f7ed9a3493ae03a
Thiago <Thiago@DESKTOP-9TMNURV>
2025-03-24 23:21:31 -0300

    teste

--- TechChallenge.Tests/Integration/ContatoAdicionar.cs
+++ TechChallenge.Tests/Integration/ContatoAdicionar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ using Xunit.Abstractions;
 
 namespace TechChallenge.Tests.Integration
 {
-
+    [ExcludeFromCodeCoverage]
     public class ContatoAdicionar : IClassFixture<ContextoFixture>
     {
         private readonly MainContext _context;
